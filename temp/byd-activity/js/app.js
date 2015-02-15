9bdb8189c0a90fe7325be07f0d3221ecddc6ef4d
liuin <20893791@qq.com>
2015-02-15 16:09:37 +0800

    0215

--- temp/byd-activity/js/app.js
+++ temp/byd-activity/js/app.js
@@ -1,20 +1,21 @@
 requirejs.config({
     //By default load any module IDs from js/lib
-    baseUrl: '../asset',
+    baseUrl: '../../asset',
     //except, if the module ID starts with "app",
     //load it from the js/app directory. paths
     //config is relative to the baseUrl, and
     //never includes a ".js" extension since
     //the paths config could be for a directory.
     paths: {
-        js: 'temp/byd-activity/js'
+        js: '../temp/byd-activity/js'
     }
 });
 
 // Start the main app logic.
-requirejs(['jquery/jquery-1.11.2.js','js/js'],
-function   ($,        canvas,   sub) {
+requirejs(['jquery/jquery-1.11.2','js/js'],
+function   ($,subjs) {
     //jQuery, canvas and the app/sub module are all
     //loaded and can be used here now.
+    var $ = $;
     console.log($);
 });
